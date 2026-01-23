0830325db5393b530942f3bf763762c7b0a2697d
Philipp Klocke <philipp.a.klocke@gmail.com>
2026-01-23 17:54:43 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Very basic haskell without any fancy dependency management.";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.systems.url = "github:nix-systems/default";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.systems.follows = "systems";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = 
+            [(pkgs.haskellPackages.ghcWithPackages (ps: with ps; [
+                # --- Dependencies ---
+                text
+                yaml
+
+                # --- Tooling ---
+                cabal-install
+                haskell-language-server
+              ]))
+            ]; 
+        };
+      }
+    );
+}
